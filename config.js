79bb6cc28e7ed96772e4bd577fb8182c8ffca7ff
lLize-huil <49223294+lLize-huil@users.noreply.github.com>
2022-01-24 17:07:09 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// 配置信息
+module.exports = {
+  fromDisplayText: '神秘人', // 收件箱展示的来件人名字
+  fromDisplaySubText: '每日邮件', // 收件箱展示的次级标题
+  user: '1841632321@qq.com', // 发送者邮箱
+  pass: 'icuqdltxvwmpdegg', // 发送者邮箱MTP协议密码
+  to: '1841632321@qq.com', // 发送到谁，填邮箱
+  weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
+  location: '101050907',  // 
+  type: '1,3,9', // 和风天气-生活指数type
+  tianXingKey: 'eb5617dab561c236480cb6ff66ccecad', // 天行数据的key
+  startDay: '2015-09-29', // 在一起的日期
+}
