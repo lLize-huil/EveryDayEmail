fe0b55a91d71dd3e03c799db31c0fe73d7479272
lLize-huil <49223294+lLize-huil@users.noreply.github.com>
2022-01-25 15:33:53 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   fromDisplaySubText: '来自辉哥的爱心邮件', // 收件箱展示的次级标题
   user: '1841632321@qq.com', // 发送者邮箱
   pass: 'icuqdltxvwmpdegg', // 发送者邮箱MTP协议密码
-  to: '1841632321@qq.com', // 发送到谁，填邮箱
+  to: '643695314@qq.com', // 发送到谁，填邮箱
   weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
   location: '101050907',  // 
   type: '1,3,9', // 和风天气-生活指数type
