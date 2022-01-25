9349878835c79f5e8b820b302031609890415488
lLize-huil <49223294+lLize-huil@users.noreply.github.com>
2022-01-25 10:39:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 // 配置信息
 module.exports = {
   fromDisplayText: '李泽辉', // 收件箱展示的来件人名字
-  fromDisplaySubText: '来自辉哥的每日爱心邮件', // 收件箱展示的次级标题
+  fromDisplaySubText: '来自辉哥的爱心邮件', // 收件箱展示的次级标题
   user: '1841632321@qq.com', // 发送者邮箱
   pass: 'icuqdltxvwmpdegg', // 发送者邮箱MTP协议密码
-//   to: '643695314@qq.com', // 发送到谁，填邮箱
   to: '1841632321@qq.com', // 发送到谁，填邮箱
   weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
   location: '101050907',  // 
   type: '1,3,9', // 和风天气-生活指数type
   tianXingKey: 'eb5617dab561c236480cb6ff66ccecad', // 天行数据的key
-  startDay: '2022-01-23', // 开始每日邮件的日期
+  LoveStartDay: '2019-01-24', // 在一起的日期
+  LogStartDay: '2022-01-24' //每日邮件开始日期
 }
